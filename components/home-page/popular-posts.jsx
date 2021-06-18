c91a82f567ec3958765ee2610971a38bc4f53fcb
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-06-18 20:02:55 +0430

    Post item dark theme

--- components/home-page/popular-posts.jsx
+++ components/home-page/popular-posts.jsx
@@ -4,7 +4,7 @@ function PopularPosts() {
   return (
     <section className="py-8 max-w-6xl">
       <div className="container mx-auto px-10 md:px-32">
-        <h1 className="mb-7 font-bold md:tracking-wide text-4xl text-gray-900">
+        <h1 className="mb-7 font-bold md:tracking-wide text-4xl text-gray-900 dark:text-gray-50">
           Popular Posts
         </h1>
 
