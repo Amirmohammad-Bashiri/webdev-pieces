bd4b29a9a148b9c92a82b9e1484bb925ca453079
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-06-18 19:53:37 +0430

    Post item structure

---
+++ components/home-page/PostItem.jsx
@@ -0,0 +1,18 @@
+function PostItem() {
+  return (
+    <div className="space-y-4">
+      <h2 className="text-lg text-gray-900">
+        Everything I Know About Style Guides, Design Systems, and Component
+        Libraries
+      </h2>
+      <p className="text-sm xl:text-base text-gray-400">Jun 17 2021</p>
+      <p className="text-base text-gray-500">
+        A deep-dive on everything I&apos;ve learned in the past year building
+        style guides, design systems, component libraries, and their best
+        practices.
+      </p>
+    </div>
+  );
+}
+
+export default PostItem;
--- components/home-page/popular-posts.jsx
+++ components/home-page/popular-posts.jsx
@@ -1,8 +1,17 @@
+import PostItem from "./PostItem";
+
 function PopularPosts() {
   return (
-    <section>
-      <div>
-        <h1>Popular POSTS!</h1>
+    <section className="py-8 max-w-6xl">
+      <div className="container mx-auto px-10 md:px-32">
+        <h1 className="mb-7 font-bold md:tracking-wide text-4xl text-gray-900">
+          Popular Posts
+        </h1>
+
+        {/* Posts */}
+        <div className="max-w-3xl">
+          <PostItem />
+        </div>
       </div>
     </section>
   );
