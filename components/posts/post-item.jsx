46c8f1cb5ca133317f5849ee7f144f9f2be2d63b
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-06-19 11:36:59 +0430

    Date format

--- components/posts/post-item.jsx
+++ components/posts/post-item.jsx
@@ -1,7 +1,9 @@
 function PostItem({ post }) {
   return (
     <li className="space-y-4">
-      <h2 className="text-lg text-gray-900 dark:text-gray-100">{post.title}</h2>
+      <h2 className="text-lg md:text-2xl text-gray-900 dark:text-gray-100">
+        {post.title}
+      </h2>
       <p className="text-sm xl:text-base text-gray-500">{post.date}</p>
       <p className="text-base text-gray-500 dark:text-gray-400">
         {post.excerpt}
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -4,7 +4,7 @@ title: "Getting Started with NextJS"
 date: "2022-10-16"
 image: getting-started-nextjs.png
 excerpt: NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.
-isFeatured: true
+isPopular: true
 ---
 
 NextJS is a **framework for ReactJS**.
