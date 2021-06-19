d5d74f374ca79de76970d43e3b102dabbb0f33dc
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-06-19 11:13:42 +0430

    Fixed the post duplication bug

--- container/posts-container.jsx
+++ container/posts-container.jsx
@@ -1,10 +1,6 @@
-import { v4 as uuidv4 } from "uuid";
-
 import PostItem from "../components/posts/post-item";
 
 function PostsContainer({ header, posts }) {
-  const postId = uuidv4();
-
   return (
     <section className="py-8 max-w-6xl">
       <div className="container mx-auto px-10 md:px-32">
@@ -12,9 +8,9 @@ function PostsContainer({ header, posts }) {
           {header}
         </h1>
 
-        <ul className="max-w-3xl">
+        <ul className="max-w-3xl space-y-5">
           {posts.map(post => (
-            <PostItem key={postId} post={post} />
+            <PostItem key={post.id} post={post} />
           ))}
         </ul>
       </div>
--- posts/getting-started-with-nextjs.md
+++ posts/getting-started-with-nextjs.md
@@ -1,9 +1,10 @@
 ---
+id: "1"
 title: "Getting Started with NextJS"
 date: "2022-10-16"
 image: getting-started-nextjs.png
 excerpt: NextJS is a the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.
-isPopular: true
+isFeatured: true
 ---
 
 NextJS is a **framework for ReactJS**.
