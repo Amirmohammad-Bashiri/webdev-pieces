1af4500d5fecccaefb529ac7985ecefa9fdf49b7
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-06-20 10:52:54 +0430

    Post detail setup

---
+++ components/posts/post-header.jsx
@@ -0,0 +1,13 @@
+function PostHeader() {
+  return (
+    <div className="space-y-6">
+      <h1 className="font-bold text-5xl tracking-wider text-gray-900">
+        Mastering Javascript in Makhmal&apos;s way
+      </h1>
+      <p className="text-base text-gray-500">October 30, 2021</p>
+      {/* the image gose here */}
+    </div>
+  );
+}
+
+export default PostHeader;
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,5 +1,13 @@
+import PostHeader from "../../components/posts/post-header";
+
 function PostDetailPage() {
-  return <div>PostDetail Page!</div>;
+  return (
+    <main className="bg-white dark:bg-black">
+      <article className="container mx-auto py-10 px-10 md:px-32">
+        <PostHeader />
+      </article>
+    </main>
+  );
 }
 
 export default PostDetailPage;
