a71c75448a001f4906006b6d3e29573635700cfc
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-06-20 11:51:32 +0430

    Post header

--- components/posts/post-header.jsx
+++ components/posts/post-header.jsx
@@ -1,11 +1,20 @@
-function PostHeader() {
+import Image from "next/image";
+
+function PostHeader({ title, date, image, slug }) {
+  const imagePath = `/images/posts/${slug}/${image}`;
+
   return (
-    <div className="space-y-6">
-      <h1 className="font-bold text-5xl tracking-wider text-gray-900">
-        Mastering Javascript in Makhmal&apos;s way
+    <div>
+      <h1 className="font-bold text-4xl md:text-5xl tracking-wider text-gray-900 dark:text-gray-50 mb-4 md:mb-6">
+        {title}
       </h1>
-      <p className="text-base text-gray-500">October 30, 2021</p>
-      {/* the image gose here */}
+      <p className="text-base text-gray-500 dark:text-gray-400 mb-5 md:mb-7">
+        {date}
+      </p>
+
+      <div className="text-center">
+        <Image src={imagePath} alt={title} width={800} height={450} />
+      </div>
     </div>
   );
 }
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,13 +1,45 @@
 import PostHeader from "../../components/posts/post-header";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function PostDetailPage() {
+function PostDetailPage({ post }) {
+  console.log(post);
   return (
     <main className="bg-white dark:bg-black">
-      <article className="container mx-auto py-10 px-10 md:px-32">
-        <PostHeader />
+      <article className="container mx-auto py-10 px-8 md:px-32">
+        <PostHeader
+          title={[post.title]}
+          date={post.date}
+          image={post.image}
+          slug={post.slug}
+        />
       </article>
     </main>
   );
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 24 * 60 * 60 * 5,
+  };
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles();
+
+  const slugs = postFileNames.map(filename => filename.replace(/.md$/, ""));
+
+  return {
+    paths: slugs.map(slug => ({ params: { slug } })),
+    fallback: false,
+  };
 }
 
 export default PostDetailPage;
