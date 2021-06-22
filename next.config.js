a62700fb9f35b25aac55ff5b334241cfe916a0d9
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-06-22 15:24:04 +0430

    Added Content-Security-Policy header

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com;
+  child-src *.youtube.com *.google.com *.twitter.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+];
+
 module.exports = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
