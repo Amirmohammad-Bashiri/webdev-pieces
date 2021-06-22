6053905cc5da0bb02f5923820022e8325eb1fad1
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-06-22 15:29:34 +0430

    Added X-Content-Type-Options, Referrer-Policy and Referrer-Policy security headers

--- next.config.js
+++ next.config.js
@@ -18,6 +18,18 @@ const securityHeaders = [
     key: "X-Frame-Options",
     value: "SAMEORIGIN",
   },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+  },
 ];
 
 module.exports = {
