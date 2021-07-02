4ce7be64ebfced85aa6e870454da67d12859c6ea
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-07-02 19:14:15 +0430

    little semantic fix

--- container/posts-container.jsx
+++ container/posts-container.jsx
@@ -4,9 +4,9 @@ function PostsContainer({ header, posts }) {
   return (
     <section className="py-8 max-w-6xl">
       <div className="container mx-auto px-10 md:px-32">
-        <h1 className="mb-7 font-bold md:tracking-wide text-4xl text-gray-900 dark:text-gray-50">
+        <h2 className="mb-7 font-bold md:tracking-wide text-4xl text-gray-900 dark:text-gray-50">
           {header}
-        </h1>
+        </h2>
 
         <ul className="max-w-3xl space-y-5">
           {posts.map(post => (
