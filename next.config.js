a52df4e9cd244e37eccfbb95a115ad9025060433
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-07-02 19:06:23 +0430

    Changed content security policy rule for font-src

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com;
   child-src *.youtube.com *.google.com *.twitter.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com;
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
-  font-src 'self';
+  font-src 'self' *.gstatic.com;
 `;
 
 const securityHeaders = [
