1aecf3168330307b7b4d7bba91145d68c25359c4
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-07-25 19:24:17 +0430

    Added some meta tags to posts

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -5,13 +5,24 @@ import PostContent from "../../components/posts/post-content";
 import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
 function PostDetailPage({ post }) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const postUrl = ``;
+
   return (
-    <main className="pt-28 md:pt-40 bg-white dark:bg-black">
+    <main className="bg-white pt-28 md:pt-40 dark:bg-black">
       <Head>
         <title>{post.title}</title>
         <meta name="description" content={post.excerpt} />
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.excerpt} />
+        <meta property="og:image" content={imagePath} />
+        <meta
+          property="og:url"
+          content={`https://webdev-nuggets.vercel.app/posts/${post.slug}`}
+        />
       </Head>
-      <div className="container mx-auto py-10 px-6 md:px-14 lg:px-32">
+      <div className="container px-6 py-10 mx-auto md:px-14 lg:px-32">
         <PostHeader
           title={[post.title]}
           date={post.date}
