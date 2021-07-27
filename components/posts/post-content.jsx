52546bc47c304402fd3500f207215b27be7c625a
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-07-27 18:50:46 +0430

    Fixed the syntax highlighter problem where it put an extra line

--- components/posts/post-content.jsx
+++ components/posts/post-content.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import materialDark from "react-syntax-highlighter/dist/cjs/styles/prism/material-dark";
+import { materialDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import jsx from "react-syntax-highlighter/dist/cjs/languages/prism/jsx";
 import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
 
@@ -41,26 +41,30 @@ function PostContent({ post }) {
       }
       const language = className.split("-")[1];
 
+      // Remove the extra new line by removing the \n at the end of the code
+      const codeData = children[children.length - 1].slice(0, -1).split();
+
       return (
         <SyntaxHighlighter
           className="overflow-x-scroll rounded"
           style={materialDark}
-          language={language}>
-          {children}
+          language={language}
+          showLineNumbers={true}>
+          {codeData}
         </SyntaxHighlighter>
       );
     },
   };
 
-  return (
-    <article className="container py-12 mx-auto">
-      <ReactMarkdown
-        className="space-y-5 text-lg prose text-gray-700 2xl:prose-xl dark:prose-dark md:space-y-7 dark:text-gray-200 lg:text-xl"
-        components={renderers}>
-        {post.content}
-      </ReactMarkdown>
-    </article>
+  const markdown = (
+    <ReactMarkdown
+      className="space-y-5 text-lg prose text-gray-700 2xl:prose-xl dark:prose-dark md:space-y-7 dark:text-gray-200 lg:text-xl"
+      components={renderers}>
+      {post.content}
+    </ReactMarkdown>
   );
+
+  return <article className="container py-12 mx-auto">{markdown}</article>;
 }
 
 export default PostContent;
