78d5d1d7d03e6c676bc87b0c2d015dc3545341e3
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2021-07-28 11:07:42 +0430

    Added preload links for font

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,28 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+            media="print"
+            onLoad="this.media='all'"
+          />
+
+          <noscript>
+            <link
+              rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
+            />
+          </noscript>
+        </Head>
         <body>
           <Main />
           <NextScript />
